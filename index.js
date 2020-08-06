4b1f6a2436215771d05f354e4733e23d9115076b
William Lin <wlin26@yahoo.com>
2020-08-06 13:46:02 -0400

    Updated menu endpoint to use caching

--- index.js
+++ index.js
@@ -6,36 +6,44 @@ const app = express();
 const Memcached = require('memcached');
 const memcached = new Memcached(`${process.env.MEMCACHED_URL}:11211`);
 
-app.get('/menu', function (req, res) {
-  console.time('test');
-  const sheets = google.sheets({ version: 'v4', auth: process.env.GOOGLE_SHEETS_API_KEY });
-  sheets.spreadsheets.values
-    .get({
-      spreadsheetId: '1wlyMhZjDg9HQJ0QjIOFNxVUpp1rjc419g7SvjKm3yyM',
-      range: 'Sheet1!A1:D150',
-    })
-    .then((response) => {
-      const keys = response.data.values.shift();
-      let formattedData = [];
-      response.data.values.forEach((row) => {
-        let dataObject = {};
-        row.forEach((val, i) => {
-          dataObject[keys[i]] = val;
+app.get('/menu', function (_req, res) {
+  memcached.get('menu', (err, data) => {
+    if (data) {
+      // Cache Hit
+      return res.status(200).send(JSON.stringify({ data }));
+    } else if (err || !data) {
+      // Cache Miss
+      const sheets = google.sheets({ version: 'v4', auth: process.env.GOOGLE_SHEETS_API_KEY });
+      sheets.spreadsheets.values
+        .get({
+          spreadsheetId: process.env.GOOGLE_SHEET_ID,
+          range: 'Sheet1!A1:D150',
+        })
+        .then((response) => {
+          const keys = response.data.values.shift();
+          let formattedData = [];
+          response.data.values.forEach((row) => {
+            let dataObject = {};
+            row.forEach((val, i) => {
+              dataObject[keys[i]] = val;
+            });
+            formattedData.push(dataObject);
+          });
+          memcached.set('menu', formattedData, 60 * 60 * 24, (err) => {
+            if (err) console.error(err);
+          });
+          return res.status(200).send(JSON.stringify({ data: formattedData }));
+        })
+        .catch((err) => {
+          console.error(err);
+          return res.status(500).send(JSON.stringify({ err }));
         });
-        formattedData.push(dataObject);
-      });
-      console.timeEnd('test');
-      return res.status(200).send(JSON.stringify({ data: formattedData }));
-    })
-    .catch((err) => {
-      console.error(err);
-      return res.status(500).send(JSON.stringify({ err }));
-    });
+    }
+  });
 });
 
-app.get('/testset', function (req, res) {
-  console.log('test set');
-  memcached.set('test', 'hello', 60, (err) => {
+app.get('/testset', function (_req, res) {
+  memcached.set('menu', 'hello', 60, (err) => {
     if (err) {
       console.error(err);
       return res.sendStatus(500);
@@ -44,9 +52,8 @@ app.get('/testset', function (req, res) {
   });
 });
 
-app.get('/testget', function (req, res) {
-  console.log('test');
-  memcached.get('test', (err, data) => {
+app.get('/testget', function (_req, res) {
+  memcached.get('menu', (err, data) => {
     if (err) {
       console.error(err);
       return res.sendStatus(500);
