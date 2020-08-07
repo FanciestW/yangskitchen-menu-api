a8ec4c0e6e44a6a37fad3dd36bad673b71aba04d
William Lin <wlin26@yahoo.com>
2020-08-07 09:53:17 -0400

    Updated index.js for deployment

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
 const Memcached = require('memcached');
-const memcached = new Memcached(`${process.env.MEMCACHED_URL}:11211`);
+const memcached = new Memcached(process.env.MEMCACHED_URL);
 
 app.get('/menu', function (_req, res) {
   memcached.get('menu', (err, data) => {
@@ -42,14 +42,32 @@ app.get('/menu', function (_req, res) {
   });
 });
 
-app.get('/testset', function (_req, res) {
-  memcached.set('menu', 'hello', 60, (err) => {
-    if (err) {
+app.get('/setmenu', function (_req, res) {
+  const sheets = google.sheets({ version: 'v4', auth: process.env.GOOGLE_SHEETS_API_KEY });
+  sheets.spreadsheets.values
+    .get({
+      spreadsheetId: process.env.GOOGLE_SHEET_ID,
+      range: 'Sheet1!A1:D150',
+    })
+    .then((response) => {
+      const keys = response.data.values.shift();
+      let formattedData = [];
+      response.data.values.forEach((row) => {
+        let dataObject = {};
+        row.forEach((val, i) => {
+          dataObject[keys[i]] = val;
+        });
+        formattedData.push(dataObject);
+      });
+      memcached.set('menu', formattedData, 60 * 60 * 24, (err) => {
+        if (err) console.error(err);
+      });
+      return res.status(200).send(JSON.stringify({ data: formattedData }));
+    })
+    .catch((err) => {
       console.error(err);
-      return res.sendStatus(500);
-    }
-    return res.sendStatus(200);
-  });
+      return res.status(500).send(JSON.stringify({ err }));
+    });
 });
 
 app.get('/testget', function (_req, res) {
@@ -57,9 +75,18 @@ app.get('/testget', function (_req, res) {
     if (err) {
       console.error(err);
       return res.sendStatus(500);
+    } else if (!data) {
+      return res.sendStatus(204);
     }
     return res.status(200).send(JSON.stringify({ data }));
   });
 });
+
+if (!process.env.SERVERLESS) {
+  const port = process.env.PORT || 8080;
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
+}
 
 module.exports.handler = serverless(app);
